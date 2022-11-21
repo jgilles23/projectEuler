3562f0ed0a239342ac54de52ad473e7dc0801f8b
jgilles23 <iisjg23@gmail.com>
2022-11-21 15:28:05 -0800

    pe113 solved

--- pe113.py
+++ pe113.py
@@ -1,13 +1,17 @@
-import numpy as np
+increasing_lookup = [[1]*10]
+decreasing_count = []
 
-increasing_lookup = [np.full(10,1)]
 
-
-E = 3
+#Number of digits is E
+E = 100
 for i in range(E):
-    increasing_lookup += [np.full(10,0)]
+    increasing_lookup += [[0]*10]
     for j in range(10):
-        increasing_lookup[-1][j] = np.sum(increasing_lookup[-2][j:])
+        increasing_lookup[-1][j] = sum(increasing_lookup[-2][j:])
+    decreasing_count += [increasing_lookup[-1][0] - 10] 
+
 for a in increasing_lookup:
     print(a)
-print("num bouncy", (10**E - 1) - (np.sum(increasing_lookup[-1][0])*2 - 10 - 1))
+print(decreasing_count)
+
+print("count non_bouncy", increasing_lookup[-1][0] + sum(decreasing_count)-1)
